02a30a16af1fc19162bc9b4a39a0ff81637c19b4
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-04-19 10:19:52 -0600

    made it slightly simpler

--- CPP/Find First Vowel Subscript.cpp
+++ CPP/Find First Vowel Subscript.cpp
@@ -16,15 +16,13 @@ int main()
 }
 int getPositionFirstVowel(string wordInput)
 {
-    string vowels = "aAeEiIoOuU";
+    const string vowels = "aAeEiIoOuU";
 	int testIfVowel = 0;
 	
 	for (int x = 0; x < wordInput.length(); ++x)
 	{
-	    size_t found = vowels.find(wordInput.substr(x, 1));
-	    if (found != string::npos)
+	    if (vowels.find(wordInput.substr(x, 1)) != string::npos)
 	    {
-	    	//cout << wordInput.substr(0, 1) << "  " << vowels[y] << "\n";
 	   		x += 100;
 		   	--testIfVowel;
 	   	}
